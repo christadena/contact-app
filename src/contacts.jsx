2a4c6e051913fe88405088b846fcbf1b4680272c
Chris Gerald Tadena <sterbenkazuto17@gmail.com>
2024-09-16 15:16:55 +0800

    first commit

---
+++ src/contacts.jsx
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Jon Snow",
+    imgURL:
+      "https://media.vanityfair.com/photos/5511bd322bf4321c293184dd/master/w_2560%2Cc_limit/jon-snow-nights-king-showdown.jpg",
+    phone: "+6390686065877",
+    email: "jonsnow@gmail.com",
+  },
+  {
+    name: "Arya Stark",
+    imgURL:
+      "https://w0.peakpx.com/wallpaper/622/844/HD-wallpaper-arya-stark-game-of-thrones-season-8-arya-stark-game-of-thrones-season-8-game-of-thrones-tv-shows.jpg",
+    phone: "+6390686074845",
+    email: "aryastark@gmail.com",
+  },
+  {
+    name: "Tyrion Lannister",
+    imgURL:
+      "https://cdn.theatlantic.com/thumbor/5-PSYrZm5xVgbaEnlbICnyiGdl8=/6x174:3312x2034/960x540/media/img/mt/2019/04/80393c4672f3dbfa94c07164fc4b90bc95f0c620a6c96900108badc1cde33d36/original.jpg",
+    phone: "+6390686074890",
+    email: "tyrionlannister@gmail",
+  },
+];
+
+export default contacts;
